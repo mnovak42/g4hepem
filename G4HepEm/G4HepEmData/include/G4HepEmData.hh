6619971081715be1130c9c0479789ae20cad2584
Mihaly Novak <mnovak@Mihalys-MacBook-Pro.local>
2021-02-01 11:32:07 +0100

    Use the same data memory layouts on the hots and device side.
    Follow Jonas (@hahnjo https://github.com/hahnjo/g4hepem/tree/header-implementations)
    suggestions for moving to use the same (electron/positron related) data memory layout
    both on the host and device sides. This maximise the re-usability of the related host
    side codes on the device.

--- G4HepEm/G4HepEmData/include/G4HepEmData.hh
+++ G4HepEm/G4HepEmData/include/G4HepEmData.hh
@@ -21,53 +21,53 @@ struct G4HepEmSBTableData;
  * @author  M. Novak
  * @date    2020
  *
- * The top level, global data structure i.e. collection of global data 
+ * The top level, global data structure i.e. collection of global data
  * structures used by all physics interactions covered by `G4HepEm`.
  *
- * There supposed to be a single instance of this data structure constructed and 
- * stored by the master `G4HepEmRunManager`. That single instance is 
- * constructed when calling the `G4HepEmRunManager::InitializeGlobal()` method 
- * wehn invoking the master `G4HepEmRunManager::Initialize()` method. 
- * Worker `G4HepEmRunManager`-s will have their pointer set to this master 
- * run-manager `G4HepEmData` member object. 
+ * There supposed to be a single instance of this data structure constructed and
+ * stored by the master `G4HepEmRunManager`. That single instance is
+ * constructed when calling the `G4HepEmRunManager::InitializeGlobal()` method
+ * wehn invoking the master `G4HepEmRunManager::Initialize()` method.
+ * Worker `G4HepEmRunManager`-s will have their pointer set to this master
+ * run-manager `G4HepEmData` member object.
  *
- * Members of this data structure, represented by their pointers, are created 
- * individualy by invoking the dedicated intialisation methods one-by-one. 
- * 
- * In case of CUDA build, the members, suffixed by `_gpu`, points do device memory 
- * locations where the corresponding data structures are located (after copying 
- * them). All the corresponding members can be (deep) copied from the host to 
- * device by calling the `CopyG4HepEmDataToGPU()` function. This will invoke 
- * the dedicated copy methods provided by the individual data structures. 
+ * Members of this data structure, represented by their pointers, are created
+ * individualy by invoking the dedicated intialisation methods one-by-one.
  *
- * The dynamically allocated memory, represented by all the members of this 
- * collection (including device side memeory as well in case of CUDA build), can 
- * be cleaned by calling the `FreeG4HepEmData()` function. This is done, in the 
+ * In case of CUDA build, the members, suffixed by `_gpu`, points do device memory
+ * locations where the corresponding data structures are located (after copying
+ * them). All the corresponding members can be (deep) copied from the host to
+ * device by calling the `CopyG4HepEmDataToGPU()` function. This will invoke
+ * the dedicated copy methods provided by the individual data structures.
+ *
+ * The dynamically allocated memory, represented by all the members of this
+ * collection (including device side memeory as well in case of CUDA build), can
+ * be cleaned by calling the `FreeG4HepEmData()` function. This is done, in the
  * `G4HepEmRunManager::Clear()` method.
 */
 
-struct G4HepEmData {  
+struct G4HepEmData {
   /** Global G4HepEmMatCutData i.e. material and scondary production threshold related data.*/
-  struct G4HepEmMatCutData*            fTheMatCutData       = nullptr; 
-  /** Global material and scondary production threshold related data.*/   
-  struct G4HepEmMaterialData*          fTheMaterialData     = nullptr;    
+  struct G4HepEmMatCutData*            fTheMatCutData       = nullptr;
+  /** Global material and scondary production threshold related data.*/
+  struct G4HepEmMaterialData*          fTheMaterialData     = nullptr;
   struct G4HepEmElementData*           fTheElementData      = nullptr;
-  
-  struct G4HepEmElectronData*          fTheElectronData     = nullptr;  
-  struct G4HepEmElectronData*          fThePositronData     = nullptr;
-  
-  struct G4HepEmSBTableData*           fTheSBTableData      = nullptr;     
 
-  
+  struct G4HepEmElectronData*          fTheElectronData     = nullptr;
+  struct G4HepEmElectronData*          fThePositronData     = nullptr;
+
+  struct G4HepEmSBTableData*           fTheSBTableData      = nullptr;
+
+
 #ifdef G4HepEm_CUDA_BUILD
-  struct G4HepEmMatCutData*            fTheMatCutData_gpu   = nullptr;  
-  struct G4HepEmMaterialData*          fTheMaterialData_gpu = nullptr;  
+  struct G4HepEmMatCutData*            fTheMatCutData_gpu   = nullptr;
+  struct G4HepEmMaterialData*          fTheMaterialData_gpu = nullptr;
   struct G4HepEmElementData*           fTheElementData_gpu  = nullptr;
 
-  struct G4HepEmElectronDataOnDevice*  fTheElectronData_gpu = nullptr;
-  struct G4HepEmElectronDataOnDevice*  fThePositronData_gpu = nullptr;
+  struct G4HepEmElectronData*          fTheElectronData_gpu = nullptr;
+  struct G4HepEmElectronData*          fThePositronData_gpu = nullptr;
 #endif  // G4HepEm_CUDA_BUILD
-  
+
 };
 
 /** Function that ...*/
@@ -80,7 +80,7 @@ void FreeG4HepEmData (struct G4HepEmData* theHepEmData);
 #ifdef G4HepEm_CUDA_BUILD
   /** Function that ...*/
   void CopyG4HepEmDataToGPU(struct G4HepEmData* onCPU);
-  
+
   /** Function that ...*/
   void FreeG4HepEmDataOnGPU(struct G4HepEmData* onCPU);
 #endif  // G4HepEm_CUDA_BUILD
--- G4HepEm/G4HepEmData/include/G4HepEmElectronData.hh
+++ G4HepEm/G4HepEmData/include/G4HepEmElectronData.hh
@@ -346,216 +346,28 @@ void FreeElectronData (struct G4HepEmElectronData** theElectronData);
 
 #ifdef G4HepEm_CUDA_BUILD
   /**
-   * Device side version of the G4HepEmElectronData structure.
-   *
-   * All G4HepEmElectronData are available on the device side as well. However,
-   * **the device side memory layout of these electron data are modified in order to facilitate
-   * coalesced memory access**. The G4HepEmElectronDataOnDevice data structure is
-   * to use to store the electron data on the main device memory with the modified
-   * memory layout.
-   */
-  struct G4HepEmElectronDataOnDevice {
-    /** Number of G4HepEm material - cuts (same as on the host side G4HepEmElectronData::fNumMatCuts).*/
-    int        fNumMatCuts;
-
-  //// === ENERGY LOSS DATA
-    /**
-     * @name Energy loss related data members:
-     * These members are used to store all continuous energy loss related data (there
-     * is a single primary kinetic energy grid for all material - cuts equally spaced
-     * in log-scale).
-     *
-     * While the common kinetic energy grid is described with the same four members
-     * and the same way as on the host side, **the host side single continuous data**
-     * G4HepEmElectronData::fELossData **array is split into five separate arrays**
-     * **on the device side** (according to the stored data types; see below) **in order to facilitate coalesced memory access**.
-     *
-     * Each of the five data arrays has a lenght of \f$N\times\f$G4HepEmElectronDataOnDevice::fNumMatCuts.
-     *   
-     * The start indices of the data, in these arrays for a material - cuts couple with the index of \f$\texttt{imc}\f$, is 
-     * \f$\texttt{iStarts}=\texttt{imc}\times N\f$.
-     * 
-     * At run-time, for a given \f$E\f$ primary kinetic energy G4HepEmElectronDataOnDevice::fELossLogMinEkin
-     * and G4HepEmElectronDataOnDevice::fELossEILDelta are used to compute the energy bin index \f$i\f$ such that
-     * \f$ E_i \leq E < E_{i+1}, i=0,\ldots,N-1\f$. Then for the given material - cuts couple index,
-     * the above \f$\texttt{iStarts}=\texttt{imc}\times N\f$ start index can be used to access the corresponding
-     * energy loss data and second derivatives associated to the primary kinetic energies of \f$ E_i, E_{i+1}\f$
-     * needed to perform the spline interpolation.
-     *    
-     */
-  ///@{
-    /** Number of discrete kinetic energy values in the grid (\f$N\f$) (same as G4HepEmElementData::fELossEnergyGridSize on the device). */
-    int        fELossEnergyGridSize;
-    /** Logarithm of the minimum kinetic energy value of the grid (\f$\ln(E_0)\f$) (same as G4HepEmElementData::fELossLogMinEkin on the device). */
-    double     fELossLogMinEkin;     // log of the E_0
-    /** Inverse of the log-scale delta value (\f$ 1/[log(E_{N-1}/E_0)/(N-1)]\f$) (same as G4HepEmElementData::fELossEILDelta on the device). */
-    double     fELossEILDelta;
-    /** The grid of the discrete kinetic energy values (\f$E_0, E_1,\ldots, E_{N-1}\f$) (same as G4HepEmElementData::fELossEnergyGrid on the device). */
-    double*    fELossEnergyGrid;
-    /** Restricted range values computed over the above primary particle kinetic energy grid
-      * for all material - cuts couples. The \f$N:=\f$G4HepEmElectronDataOnDevice::fELossEnergyGridSize
-      * range values for each material - cuts couple are stored continuously in this array.*/
-    double*    fELossDataRange;
-    /** The second derivative values associated to the reange values stored in the above G4HepEmElectronDataOnDevice::fELossDataRange array.*/
-    double*    fELossDataRangeSD;
-    /** Restricted dE/dx values computed over the above primary particle kinetic energy grid
-      * for all material - cuts couples. The \f$N:=\f$G4HepEmElectronDataOnDevice::fELossEnergyGridSize
-      * dE/dx values for each material - cuts couple are stored continuously in this array.*/
-    double*    fELossDataDEDX;
-    /** The second derivative values associated to the dE/dx values stored in the above G4HepEmElectronDataOnDevice::fELossDataDEDX array.*/
-    double*    fELossDataDEDXSD;
-    /** The second derivative values associated to the inverse-reange values (range and kinetic energy grid values stored in the arrays above).*/
-    double*    fELossDataInvRangeSD;
-  /// @} */ end: eloss data
-
-  //// ===  Restricted macroscopic cross section data: ioni and brem
-    /**
-     * @name Restricted macroscopic cross section related data members:
-     * These members are used to store all restricted macroscopic cross section related data both for
-     * **ionisation** and **bremsstrahlung** for all material - cuts couples.
-     *
-     * While all the data are stored continuously in the G4HepEmElectronData::fResMacXSecData **single array on the host side**,
-     * this is **split into sevar arrays on the device side in order to facilitate coalesced memory access**: 
-     *   - data are stored separately for ionisation and for bremsstrahlung
-     *   - one array per interaction stores the \f$M\f$ number of \f$E_0, E_i,\ldots,E_{M-1}\f$ primary particle kinetic 
-     *     energy grid points for each material - cuts couple, used to compute and store the given \f$\Sigma(E_i)\f$ restricted macroscopic cross section
-     *     for the given material - cuts couple
-     *   - the four auxiliary data (\f$\texttt{argmax}\{\Sigma(E_i)\}, \texttt{max}\{\Sigma(E_i)\}, \log(E_0), 1/[log(E_{M-1}/E_0)/(M-1)]\f$) are stored
-     *     in a separate array for each interactions and for each material - cuts
-     *   - the three data, that are needed for the run - time spline interpolations, \f$E_i, \Sigma(E_i), \Sigma(E_i)^{''}\f$ are stored
-     *     in three different separate arrays for a given interaction for all material - cuts couples
-     *   - an additinal array is used for each interaction to store the index where the above data starts in the three arrays
-     * 
-     * At run-time, for a given \f$E\f$ primary kinetic energy and G4HepEmMCCData material - cuts couple data index of \f$\texttt{imc}\f$, 
-     * first the kinetic energy bin index is computed. This can be done by using the corrsponding auxiliary data \f$\log(E_0),1/[log(E_{M-1}/E_0)/(M-1)]\f$ 
-     * that are stored at the indices of \f$4\times\f$G4HepEmElectronDataOnDevice::fNumMatCuts\f$+2\f$ and \f$+3\f$ in the corresonding 
-     * \f$\texttt{fResMacXSecXYAuxData}\f$ array (where \f$\texttt{XY}\in \{\texttt{Ioni,Brem}\}\f$). The required kinetic energy bin index \f$i\f$, 
-     * such that \f$ E_i \leq E < E_{i+1}, i=0,\ldots,M-1\f$, where \f$M\f$ is the number of kinetic energy values for the given \f$\texttt{XY}\f$ interaction and 
-     * material - cuts couple \f$\texttt{imc}\f$ stored in the corresponding \f$\texttt{fResMacXSecNumXYData[imc]}\f$, can be computed.
-     * After having the kinetic energy bin index, the appropriate three data arrays can be used to obtain the \f$E_i, \Sigma(E_i), \Sigma(E_i)^{''}, E_{i+1}, \Sigma(E_{i+1}), \Sigma(E_{i+1})^{''}\f$ data required for 
-     * the spline interpolation. These data are at the \f$i\f$-th and \f$i+1\f$-th positions in the given arrays relative to the start index for the given material - cuts couple 
-     * stored in the correspondig \f$\texttt{fResMacXSecXYDataStart[imc]}\f$.
-     *
-     */
-  ///@{
-    /** Start index of the *ionisation* data in the G4HepEmElectronDataOnDevice::fResMacXSecIoniData array per material - cuts couple.*/
-    int*       fResMacXSecIoniDataStart; // ioni data first index in fResMacXSecIoniData for mat-cut; [#material-cuts couple]
-    /** Number of discrete kinetic energy grid points at which macroscopic cross section data are stored for *ionisation* in the G4HepEmElectronDataOnDevice::fResMacXSecIoniEData, G4HepEmElectronDataOnDevice::fResMacXSecIoniData, G4HepEmElectronDataOnDevice::fResMacXSecIoniSDData  arrays 
-      * per material - cuts couple.*/
-    int*       fResMacXSecNumIoniData;   // #ioni data for the given mat-cut; [#material-cuts couple]
-    /** Same as above for *bremsstrahlung*. */
-    int*       fResMacXSecBremDataStart; // same as above for brem
-    /** Same as above for *bremsstrahlung*. */
-    int*       fResMacXSecNumBremData;   // same as above for brem
-
-    /** Array to store the four auxiliary \f$\texttt{argmax}\{\Sigma(E_i)\}, \texttt{max}\{\Sigma(E_i)\}, \log(E_0), 1/[log(E_{M-1}/E_0)/(M-1)]\f$ data for ionisation per material - cuts couples. */
-    double*    fResMacXSecIoniAuxData;   // energyOfMaxVal, maxVal, logEMin, invLogEDelta; [4 x #material-cuts couple]
-    /** Data array that stores the kinetic energy grids for *ionisation* for all material - cuts couples. Start index for a given material - cuts are stored in G4HepEmElectronDataOnDevice::fResMacXSecIoniDataStart.*/ 
-    double*    fResMacXSecIoniEData;     // ioni mac-xsec energy grid
-    /** Data array that stores the restricted macroscopic cross sections for *ionisation*, at discrete primary kinetic energies stored in the above array, for all material - cuts couples. Start index for a given material - cuts are stored in G4HepEmElectronDataOnDevice::fResMacXSecIoniDataStart.*/ 
-    double*    fResMacXSecIoniData;      // ioni mac-xsec values
-    /** Data array that stores the second derivatives of the above  macroscopic cross sections for *ionisation*, for all material - cuts couples. Start index for a given material - cuts are stored in G4HepEmElectronDataOnDevice::fResMacXSecIoniDataStart.*/ 
-    double*    fResMacXSecIoniSDData;    // ioni mac-xsec second derivatives
-
-    /** Same as above for *bremsstrahlung*.*/
-    double*    fResMacXSecBremAuxData;   // same as above for brem
-    /** Same as above for *bremsstrahlung*.*/
-    double*    fResMacXSecBremEData;     //
-    /** Same as above for *bremsstrahlung*.*/
-    double*    fResMacXSecBremData;      //
-    /** Same as above for *bremsstrahlung*.*/
-    double*    fResMacXSecBremSDData;    //
-  /// @} */ end: macroscopic cross sections
-
-  //// ===   Target element selector data: for the ioni (Moller-Bhabha) and brem models (Seltzer-Berger, Relativistic)
-    /**
-     * @name Target element selector related data members:
-     * These members store data utilised at run-time for the selection of the target
-     * elements (in case of multi element atoms) on which the interaction takes place.
-     * Data are stored for all the interaction models used to describe both **ionisation** and
-     * **bremsstrahlung** and for all material - cuts couples.
-     *
-     * The **host side** \f$\texttt{fElemSelectorXYData}\f$ **single data array for a given** \f$\texttt{XY} \in \{Ioni,BremSB, BremRB\}\f$ **interaction**
-     * **model**, used to store the target element selector related data for all material - cuts couple for the given intercation model, 
-     * **is split into three arrays on the device side in order to facilitate coalesced memory access**:
-     *   - \f$fElemSelectorNumXYData\f$ array stores the \f$K\f$ number of discrte primary kinetic energy values used to compute the elements-wise data for the \f$XY \in \{Ioni,BremSB, BremRB\}\f$ interaction model, per material - cuts couples.
-     *   - \f$fElemSelectorXYAuxData\f$ array stores the two \f$\log(E_0), 1/[log(E_{K-1}/E_0)/(K-1)]\f$ auxiliary data needed for the kinetic energy bin computations for the \f$XY \in \{Ioni,BremSB, BremRB\}\f$ interaction model, per material - cuts couples.
-     *   - \f$fElemSelectorXYData\f$ array stores the normalised, element-wise contributions to the macroscopic cross sections over the discrete kinetic energy grid \f$E_0,E_1,\ldots,E_{K-1}\f$ in the format of
-     *       - \f$E_0,P(j=0,E_0),P(j=1,E_0),\ldots,P(j=Q-2,E_0), \ldots,\f$ \f$E_{K-1},P(j=0,E_{K-1}),P(j=1,E_{K-1}),\ldots,P(j=Q-2,E_{K-1})\f$
-     *       - where \f$j=0,\ldots,Q-2\f$ is element index with \f$Q\f$ being the total number of elements the material is composed of that is stored in the G4HepEmElectronDataOnDevice::fElemSelectorNumElements per material - cuts couples.
-     *       
-     * An additional \f$fElemSelectorXYDataStar\f$ array is used in each case of \f$\texttt{XY} \in \{Ioni,BremSB, BremRB\}\f$ interaction models 
-     * to store the start indices of the data in the corresonding \f$fElemSelectorXYData\f$  array per material - cuts couples.
-     *
-     * At run-time, for a given interaction model \f$\texttt{XY} \in \{Ioni,BremSB, BremRB\}\f$, \f$E\f$ primary kinetic energy and G4HepEmMCCData material - cuts couple data index of \f$\texttt{imc}\f$, 
-     * first the kinetic energy bin index is computed. This can be done by using the corrsponding auxiliary data \f$\log(E_0),1/[log(E_{K-1}/E_0)/(K-1)]\f$ 
-     * that stored at the indices of \f$2\times\f$G4HepEmElectronDataOnDevice::fNumMatCuts\f$\f$ and \f$+1\f$ in the corresonding 
-     * \f$\texttt{fElemSelectorXYAuxData}\f$ array. The required kinetic energy bin index \f$i\f$, 
-     * such that \f$ E_i \leq E < E_{i+1}, i=0,\ldots,K-1\f$, where \f$K\f$ is the number of kinetic energy values for the given \f$\texttt{XY}\f$ interaction and 
-     * material - cuts couple \f$\texttt{imc}\f$ stored in the corresponding \f$\texttt{fElemSelectorNumXYData[imc]}\f$, can be computed.
-     * After having the kinetic energy bin index, the appropriate \f$fElemSelectorXYData\f$ array can be used to obtain the data \f$E_i,P(j=0,\ldots,Q-2;E_i)\f$ and \f$E_{i+1},P(j=0,\ldots,Q-2;E_{i+1})\f$ needed for the (linear) interpolation and element selection.
-     * These data start from the \f$i \to i\times Q\f$-th and \f$i+1 \to (i+1)\times Q\f$-th positions in the given array, relative to the start index for the given material - cuts couple 
-     * stored in at correspondig \f$\texttt{fElemSelectorXYDataStart[imc]}\f$ for this material - cuts couple with the index of \f$\texttt{imc}\f$.
-     */
-    /** Number of elements the multi element material - cuts couples composed of per material - cuts.*/ 
-    int*       fElemSelectorNumElements;
-    // - start of element selector data and number of energy bins
-    //   for each material cuts couple (and for Moller-Bhabha ioni and Seltzer-Berger
-    //   Rel. bren models)
-    
-    /** Start index of element selector data for ionisation (Moller-Bhabha) interaction model per material - cuts couple.*/
-    int*       fElemSelectorIoniDataStart;    // [#mat-cuts]
-    /** Number of discrete kinetic energy values used to store element selector data for ionisation (Moller-Bhabha) interaction model per material - cuts couple.*/
-    int*       fElemSelectorNumIoniData;
-    
-    /** Start index of element selector data for bremsstrahlung (Seltzer-Berger) interaction model per material - cuts couple.*/
-    int*       fElemSelectorBremSBDataStart;
-    /** Number of discrete kinetic energy values used to store element selector data for bremsstrahlung (Seltzer-Berger) interaction model per material - cuts couple.*/
-    int*       fElemSelectorNumBremSBData;
-    
-    /** Start index of element selector data for bremsstrahlung (Relativistic) interaction model per material - cuts couple.*/
-    int*       fElemSelectorBremRBDataStart;
-    /** Number of discrete kinetic energy values used to store element selector data for bremsstrahlung (Relativistic) interaction model per material - cuts couple.*/
-    int*       fElemSelectorNumBremRBData;
-
-    /** Array to store the \f$\log(E_0), 1/[log(E_{K-1}/E_0)/(K-1)]\f$ auxiliary data used to compute the kinetic energy bin index for (Moller-Bhabha) interaction model per material - cuts couple.*/
-    double*    fElemSelectorIoniAuxData;
-    /** Same as above for bremsstrahlung (Seltzer-Berger) interaction model.*/
-    double*    fElemSelectorBremSBAuxData;
-    /** Same as above for bremsstrahlung (Relativistic) interaction model.*/
-    double*    fElemSelectorBremRBAuxData;
-
-    /** Element selector data, discrete kinetic energy values and normalised, element-wise contributions to the macroscopic cross sections, 
-      * for ionisation (Moller-Bhabha) interaction model for all material - cuts couples.*/
-    double*    fElemSelectorIoniData;
-    /** Same as above for bremsstrahlung (Seltzer-Berger) interaction model.*/
-    double*    fElemSelectorBremSBData;
-    /** Same as above for bremsstrahlung (Relativistic) interaction model.*/
-    double*    fElemSelectorBremRBData;
-  };
-
-  /**
     * Allocates memory for and copies the G4HepEmElectronData structure from the
     * host to the device.
     *
-    * The input arguments are supposed to be the corresponding members of the 
-    * G4HepEmData, top level data structure, stored in the `master` G4HepEmRunManager.    
+    * The input arguments are supposed to be the corresponding members of the
+    * G4HepEmData, top level data structure, stored in the `master` G4HepEmRunManager.
     *
     * @param onHOST    pointer to the host side, already initialised G4HepEmElectronData structure.
-    * @param onDEVICE  host side address of a G4HepEmElectronDataOnDevice structure memory pointer. The pointed 
-    *   memory is cleaned (if not null at input) and points to the device side memory at termination 
-    *   that stores the copied G4HepEmElectronDataOnDevice structure.
-    */  
-  void CopyElectronDataToDevice(struct G4HepEmElectronData* onHOST, struct G4HepEmElectronDataOnDevice** onDEVICE);
+    * @param onDEVICE  host side address of a pointer to a device side G4HepEmElectronData
+    *   structure. The pointed device side memory is cleaned (if not null at input) and
+    *   points to the device side memory at termination containing all the copied
+    *   G4HepEmElectronData structure members.
+    */
+  void CopyElectronDataToDevice(struct G4HepEmElectronData* onHOST, struct G4HepEmElectronData** onDEVICE);
 
   /**
-    * Frees all memory related to the device side G4HepEmElectronDataOnDevice structure referred 
+    * Frees all memory related to the device side G4HepEmElectronData structure referred
     * by the pointer stored on the host side input argument address.
     *
     * @param onDEVICE host side address of a G4HepEmElectronDataOnDevice structure located on the device side memory.
     *   The correspondig device memory will be freed and the input argument address will be set to null.
     */
-  void FreeElectronDataOnDevice(struct G4HepEmElectronDataOnDevice** onDEVICE);
+  void FreeElectronDataOnDevice(struct G4HepEmElectronData** onDEVICE);
 #endif // DG4HepEm_CUDA_BUILD
 
 
