b6c20eac57d4dcb01523239677172def423ce4a3
Mihaly Novak <mihaly.novak@cern.ch>
2021-12-13 16:50:16 +0100

    Add the new fast Log and Exp functions as specialisations of the G4HepEmLog and G4HepEmExp functions.

--- G4HepEm/G4HepEmRun/include/G4HepEmMath.hh
+++ G4HepEm/G4HepEmRun/include/G4HepEmMath.hh
@@ -4,6 +4,9 @@
 
 #include <cmath>
 
+#include "G4HepEmExp.hh"
+#include "G4HepEmLog.hh"
+
 #include "G4HepEmMacros.hh"
 
 template <typename T>
@@ -22,6 +25,40 @@ template <typename T>
 G4HepEmHostDevice static inline
 T G4HepEmX13(T x) {
  return std::pow(x, 1./3.);
+}
+
+// --- Log function with VDT (G4Log) specialisations for double and float
+template <typename T>
+G4HepEmHostDevice inline
+T G4HepEmLog(T x) {
+ return std::log(x);
+}
+template < >
+G4HepEmHostDevice inline
+double G4HepEmLog(double x) {
+ return VDTLog(x);
+}
+template < >
+G4HepEmHostDevice inline
+float G4HepEmLog(float x) {
+ return VDTLogf(x);
+}
+
+// --- Exp function with VDT (G4Exp) specialisations for double and float
+template <typename T>
+G4HepEmHostDevice inline
+T G4HepEmExp(T x) {
+ return std::exp(x);
+}
+template < >
+G4HepEmHostDevice inline
+double G4HepEmExp(double x) {
+ return VDTExp(x);
+}
+template < >
+G4HepEmHostDevice inline
+float G4HepEmExp(float x) {
+ return VDTExpf(x);
 }
 
 #endif // G4HepEmMath_HH
