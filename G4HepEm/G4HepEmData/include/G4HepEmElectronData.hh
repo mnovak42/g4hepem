5f966abc4ab0e93e7e05bab30d7858284b957175
Mihaly Novak <mihaly.novak@cern.ch>
2024-11-21 10:12:31 +0100

    Increase the grid density of the electron nuclear cross sections.

--- G4HepEm/G4HepEmData/include/G4HepEmElectronData.hh
+++ G4HepEm/G4HepEmData/include/G4HepEmElectronData.hh
@@ -236,10 +236,10 @@ struct G4HepEmElectronData {
 
   //
   // Electron - and positron - nuclear cross sections per material:
-  // --- Grid: 63 bins form 100 MeV - 100 TeV
-  const int     fENucEnergyGridSize = 64;
+  // --- Grid: 127 bins form 100 MeV - 100 TeV
+  const int     fENucEnergyGridSize = 128;
   double        fENucLogMinEkin = 0.0;    // = 4.605170185988092;  // log(100.0)
-  double        fENucEILDelta = 0.0;      // = 4.560092059984145;  // 1./[log(emax/emin)/63]
+  double        fENucEILDelta = 0.0;      // = 9.192566533618830;  // 1./[log(emax/emin)/127]
   double*       fENucEnergyGrid = nullptr;    // [fENucEnergyGrid]
 
   double*       fENucMacXsecData = nullptr;   // [#materials*2*fGNucEnergyGridSize]
