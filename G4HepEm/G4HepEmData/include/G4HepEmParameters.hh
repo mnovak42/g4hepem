bc8fbf30782b9f142e36ac442d28bb75a4ca47a5
Mihaly Novak <mihaly.novak@cern.ch>
2025-01-25 14:54:19 +0100

    First step moving to having some configuration parameters per region.

--- G4HepEm/G4HepEmData/include/G4HepEmParameters.hh
+++ G4HepEm/G4HepEmData/include/G4HepEmParameters.hh
@@ -17,43 +17,100 @@
  * G4HepEmRunManager when its InitializeGlobal() method is invoked by calling
  * the InitHepEmParameters() function declared in the G4HepEmParamatersInit
  * header file. This method extracts information (mainly) from the G4EmParameters
- * singletone object.
+ * singletone object. Therefore, the default values given here will be updated
+ * during the initialisation.
  */
+
+/** Parameters per detector region. */
+struct G4HepEmRegionParmeters {
+  /** The *final range* parameter of the sub-threshold energy loss related step limit function.*/
+  double fFinalRange = 1.0;
+  /** The *rover range* parameter of the sub-threshold energy loss related step limit function.*/
+  double fDRoverRange = 0.2;
+  /** Maximum allowed *linear* energy loss along step due to sub-threshold (continuous) energy losses
+    * given as fraction of the intial kinetic energy. Proper integral is used to compute the mean energy loss
+    * when the energy loss, according to linear approximation, is over this threshold.*/
+  double fLinELossLimit = 0.01;
+
+  /** MSC range and safety factor parameters */
+  double fMSCRangeFactor  = 0.04;
+  double fMSCSafetyFactor = 0.6;
+  /** Flag to indicate if the non-default, simplified `fMinimal` MSC step limit should be used.*/
+  bool   fIsMSCMinimalStepLimit = false;
+
+  /** Flag to indicate if energy loss fluctuation should be used.*/
+  bool   fIsELossFluctuation = true;
+
+  /** Flag to indicate if the combined MSC + Transportation process is allowed for multiple steps. */
+  bool   fIsMultipleStepsInMSCTrans = true;
+};
+
 
 struct G4HepEmParameters {
   /** \f$e^-/e^+\f$ tracking (kinetic) energy cut in Geant4 internal energy units:
     * \f$e^-/e^+\f$ tracks are stopped when their energy drops below this threshold,
     * their kinetic energy is deposited and annihilation to two \f$\gamma\f$-s interaction
     * is invoked for in case of \f$e^+\f$.*/
-  double fElectronTrackingCut;
+  double fElectronTrackingCut = 0.001;
 
   // The configuration of the kinetic energy grid of the energy loss related tables:
   /** Minimum of the kinetic energy grid used to build the sub-(secondary-production)threshold
     * related energy loss quantity tables such as the *restricted stopping power*, *range* and
     * *inverse range* tables. */
-  double fMinLossTableEnergy;
+  double fMinLossTableEnergy = 0.0001; // 100 eV
   /** Maximum of the kinetic energy grid for loss tables.*/
-  double fMaxLossTableEnergy;
+  double fMaxLossTableEnergy = 1.0E+08; // 100 TeV
   /** Number of bins (equally spaced on log scale) of the loss table kinetic energy grid. */
-  int    fNumLossTableBins;
-
-  /** The *final range* parameter of the sub-threshold energy loss related step limit function.*/
-  double fFinalRange;
-  /** The *rover range* parameter of the sub-threshold energy loss related step limit function.*/
-  double fDRoverRange;
-  /** Maximum allowed *linear* energy loss along step due to sub-threshold (continuous) energy losses
-    * given as fraction of the intial kinetic energy. Proper integral is used to compute the mean energy loss
-    * when the energy loss, according to linear approximation, is over this threshold.*/
-  double fLinELossLimit;
+  int    fNumLossTableBins   = 84;
 
   /** Kinetic energy limit between the two (Seltzer-Berger and Relativistic) models for bremsstrahlung photon emission
     * in case of \f$e^-/e^+\f$ primary particles.*/
-  double fElectronBremModelLim;
+  double fElectronBremModelLim = 1000; // 1 GeV
+
+  /** Number of detector regions */
+  int fNumRegions = 0;
+  /** A `G4HepEmRegionParmeters` array for the individual detector regions. */
+  G4HepEmRegionParmeters* fParametersPerRegion = nullptr; //[fNumRegions]
+
+#ifdef G4HepEm_CUDA_BUILD
+  G4HepEmRegionParmeters* fParametersPerRegion_gpu = nullptr; //[fNumRegions]
+#endif  // G4HepEm_CUDA_BUILD
+
+
+
+
+  /** The *final range* parameter of the sub-threshold energy loss related step limit function.*/
+  double fFinalRange = 1.0;
+  /** The *rover range* parameter of the sub-threshold energy loss related step limit function.*/
+  double fDRoverRange = 0.2;
+  /** Maximum allowed *linear* energy loss along step due to sub-threshold (continuous) energy losses
+    * given as fraction of the intial kinetic energy. Proper integral is used to compute the mean energy loss
+    * when the energy loss, according to linear approximation, is over this threshold.*/
+  double fLinELossLimit = 0.01;
 
   // MSC range and safety factor parameters
-  double fMSCRangeFactor;
-  double fMSCSafetyFactor;
+  double fMSCRangeFactor  = 0.04;
+  double fMSCSafetyFactor = 0.6;
+
 
 };
+
+/** Function that ...*/
+void InitG4HepEmParameters (struct G4HepEmParameters* theHepEmParams);
+
+/** Function that ...*/
+void FreeG4HepEmParameters (struct G4HepEmParameters* theHepEmParams);
+
+
+#ifdef G4HepEm_CUDA_BUILD
+  /** Function that makes the `G4HepEmRegionParmeters` array member of `G4HepEmParameters`
+    * available on the device (the host side `_gpu` pointer will refer to the device side array).*/
+  void CopyG4HepEmParametersToGPU(struct G4HepEmParameters* onCPU);
+
+  /** Function that ...*/
+  void FreeG4HepEmParametersOnGPU(struct G4HepEmParameters* onCPU);
+#endif  // G4HepEm_CUDA_BUILD
+
+
 
 #endif // G4HepEmParameters_HH
--- G4HepEm/G4HepEmInit/src/G4HepEmParametersInit.cc
+++ G4HepEm/G4HepEmInit/src/G4HepEmParametersInit.cc
@@ -5,10 +5,15 @@
 
 // g4 include
 #include "G4EmParameters.hh"
+#include "G4RegionStore.hh"
+#include "G4MscStepLimitType.hh"
 #include "G4SystemOfUnits.hh"
 
+// NOTE: here we set all parameters according to their Geant4 values then
+//       later, in the `G4HepEmRunManager` from where this method is invoked from,
+//       we apply possible per-region configurations on the top of this.
 void InitHepEmParameters(struct G4HepEmParameters* hepEmPars) {
-  // e-/e+ tracking cut in kinetic energy
+  // tracking cut for e- in internal Geant4 energy units
   hepEmPars->fElectronTrackingCut = G4EmParameters::Instance()->LowestElectronEnergy();
 
   // energy loss table (i.e. dE/dx) related paramaters
@@ -16,15 +21,41 @@ void InitHepEmParameters(struct G4HepEmParameters* hepEmPars) {
   hepEmPars->fMaxLossTableEnergy   = G4EmParameters::Instance()->MaxKinEnergy();
   hepEmPars->fNumLossTableBins     = G4EmParameters::Instance()->NumberOfBins();
 
-  hepEmPars->fFinalRange           = 1.0*CLHEP::mm;
-  hepEmPars->fDRoverRange          = 0.2;
-  hepEmPars->fLinELossLimit        = G4EmParameters::Instance()->LinearLossLimit();
-
   // e-/e+ related auxilary parameters:
   // energy limit between the 2 models (Seltzer-Berger and RelBrem) used for e-/e+
   hepEmPars->fElectronBremModelLim = 1.0*CLHEP::GeV;
 
+  // get the number of detector regions and allocate the per-region data array
+  int numRegions = G4RegionStore::GetInstance()->size();
+  hepEmPars->fNumRegions = numRegions;
+  hepEmPars->fParametersPerRegion = new G4HepEmRegionParmeters[numRegions];
+
+  // set default values for all regions (might be changed after this init)
+  for (int i=0; i<G4RegionStore::GetInstance()->size(); ++i) {
+    G4HepEmRegionParmeters& rDat = hepEmPars->fParametersPerRegion[i];
+    // std::cout << " [" << i << "] Regin name = " << (*G4RegionStore::GetInstance())[i]->GetName() << std::endl;
+
+    // NOTE: these values are hidden inside G4EmParameters::G4EmExtraParameters!!!
+    rDat.fFinalRange    = 1.0*CLHEP::mm;
+    rDat.fDRoverRange   = 0.2;
+    rDat.fLinELossLimit = G4EmParameters::Instance()->LinearLossLimit();
+
+    rDat.fMSCRangeFactor  = G4EmParameters::Instance()->MscRangeFactor();
+    rDat.fMSCSafetyFactor = G4EmParameters::Instance()->MscSafetyFactor();
+
+    rDat.fIsMSCMinimalStepLimit = (G4MscStepLimitType::fMinimal == G4EmParameters::Instance()->MscStepLimitType());
+
+    rDat.fIsELossFluctuation = G4EmParameters::Instance()->LossFluctuation();
+
+    rDat.fIsMultipleStepsInMSCTrans = true;
+  }
+
+  hepEmPars->fFinalRange           = 1.0*CLHEP::mm;
+  hepEmPars->fDRoverRange          = 0.2;
+  hepEmPars->fLinELossLimit        = G4EmParameters::Instance()->LinearLossLimit();
+
   // range factor parameter of the MSC stepping
   hepEmPars->fMSCRangeFactor       = G4EmParameters::Instance()->MscRangeFactor();
   hepEmPars->fMSCSafetyFactor      = G4EmParameters::Instance()->MscSafetyFactor();
+
 }
